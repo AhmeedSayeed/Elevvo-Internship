18d16c3eee5daae1d1fabfd557134e8dedbd267c
Ahmed <as3992054@gmail.com>
2025-09-01 20:41:28 +0300

    Responsive the feature component

--- Tasks/LandingPage/src/components/Feature.jsx
+++ Tasks/LandingPage/src/components/Feature.jsx
@@ -1,6 +1,6 @@
 export default function Feature({ description, image }) {
-    return <div className="bg-white/20 backdrop-blur-md p-4 rounded-lg shadow-lg text-center flex flex-col items-center gap-4 w-[400px] h-[500px] hover:-translate-y-4 transition-all duration-400 ease-in-out">
-        <img src={image} alt="feature" className="w-full h-[350px] object-cover rounded-2xl" />
+    return <div className="bg-white/20 backdrop-blur-md p-4 rounded-lg shadow-lg text-center flex flex-col items-center gap-4 max-sm:w-[300px] max-sm:h-[400px] w-[400px] h-[500px] hover:-translate-y-4 transition-all duration-400 ease-in-out">
+        <img src={image} alt="feature" className="w-full max-sm:h-[250px] h-[350px] object-cover rounded-2xl" />
         <p className="text-lg text-muted">{description}</p>
     </div>
 }
