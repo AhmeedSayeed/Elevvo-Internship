317b6a4400eaa1af9c80bffcd857079e9031834d
Ahmed <as3992054@gmail.com>
2025-09-04 15:23:09 +0300

    add forgotten error, loading responsivness.

--- Tasks/WeatherDashboard/src/components/Error.jsx
+++ Tasks/WeatherDashboard/src/components/Error.jsx
@@ -1,5 +1,5 @@
 export default function Error({ error }) {
-    return <div className="flex flex-col items-center justify-center w-[60%] h-[60vh] bg-white/20 backdrop-blur-md shadow-lg rounded-2xl p-16">
+    return <div className="flex flex-col items-center justify-center w-[60%] max-sm:h-[35vh] max-md:h-[45vh] h-[60vh] bg-white/20 backdrop-blur-md shadow-lg rounded-2xl p-16">
         <h3 className="text-2xl text-red-500 text-center font-bold uppercase">{error}</h3>
     </div>;
 }
--- Tasks/WeatherDashboard/src/components/Loading.jsx
+++ Tasks/WeatherDashboard/src/components/Loading.jsx
@@ -1,8 +1,8 @@
 export default function Loading() {
-    return <div className="flex flex-col items-center justify-center w-[60%] h-[60vh] bg-white/20 backdrop-blur-md shadow-lg rounded-2xl p-16">
+    return <div className="flex flex-col items-center justify-center max-sm:w-[80%] w-[60%] max-sm:h-[35vh] max-md:h-[45vh] h-[60vh] bg-white/20 backdrop-blur-md shadow-lg rounded-2xl p-16">
         <div className="flex flex-col gap-2 items-center space-y-4">
                 <div className="w-16 h-16 border-4 border-t-gray-500 border-gray-300 rounded-full animate-spin"></div>
-                <div className="text-body text-xl font-semibold">Loading weather data...</div>
+                <div className="text-body text-xl font-semibold text-center">Loading weather data...</div>
             </div>
     </div>;
 }
